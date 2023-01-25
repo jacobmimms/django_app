cb3aa759e32fb78e6578d8c701641733508228b4
Jacob Mimms <jacobmimms@Jacobs-MacBook-Pro.local>
2023-01-25 12:11:31 +0100

    bad physics

--- blog/static/blog/threeCode.js
+++ blog/static/blog/threeCode.js
@@ -1,3 +1,6 @@
+const G = 32.81;
+let camera_data = {position: new THREE.Vector3(0, 5, 0), rot: new THREE.Vector3(0, 0, 0), vel: new THREE.Vector3(0, 0, 0), acc: new THREE.Vector3(0, 0, 0)}
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer({
@@ -9,7 +12,7 @@ renderer.domElement.style.height = '100%';
 const axesHelper = new THREE.AxesHelper( 5 );
 scene.add( axesHelper );
 
-camera.position.set(0, 5, 0);
+camera.position.set(0, 10, 0);
 
 const PlaneGeometry = new THREE.PlaneGeometry(1000, 1000);
 const planeMaterial = new THREE.MeshStandardMaterial({color: 0xffffff});
@@ -25,14 +28,15 @@ torus.translateZ(-100)
 torus.translateY(15)
 scene.add( torus );
 
-const light = new THREE.PointLight(0xffffff, 20, 100);
-light.position.set(0, 90, );
+const light = new THREE.PointLight(0xffffff, 20, 20);
+light.position.set(0, 90, 0);
 scene.add(light); 
 const sphereSize = 1;
 const pointLightHelper = new THREE.PointLightHelper( light, sphereSize );
 scene.add( pointLightHelper );
 
 scene.background = new THREE.Color(0x333333);
+
 function resizeCanvasToDisplaySize() {
 	const canvas = renderer.domElement;
 	const width = canvas.clientWidth;
@@ -43,8 +47,6 @@ function resizeCanvasToDisplaySize() {
 	  camera.updateProjectionMatrix();
 	}
 }
-
-
 
 $(document).keydown(function(event) {
 	startMove(event);
@@ -57,34 +59,46 @@ $(document).keyup(function(event) {
 const startMove = function(input) {
 	let key = input.key;
 	inputs[key] = true;
-	console.log(key)
-	console.log(inputs[key])
 }
 
 const stopMove = function(input) {
 	let key = input.key;
 	inputs[key] = false;
-	console.log(key)
-	console.log(inputs[key])
 }
-
 const moveForward = function() {
-	camera.position.z -= 1;
+	camera_data.position.z -= 1;
 }
 const moveBackward = function() {
-	camera.position.z += 1;
+	camera_data.position.z += 1;
 }
 const moveLeft = function() {
-	camera.position.x -= 1;
+	camera_data.position.x -= 1;
 }
 const moveRight = function() {
-	camera.position.x += 1;
+	camera_data.position.x += 1;
 }
 const moveUp = function() {
-	camera.position.y += 1;
+	camera_data.position.y += 1;
 }
 const moveDown = function() {
-	camera.position.y -= 1;
+	camera_data.position.y -= 1;
+}
+const gravity = function(delta_time) {
+	if (camera_data.position.y <= 5) {
+		camera_data.acc.y = 0;
+		camera_data.vel.y = 0;
+		camera_data.position.y = 5;
+	}
+	camera_data.position.y += camera_data.vel.y ;
+	camera_data.vel.y += camera_data.acc.y ;
+	camera_data.acc.y += (-G * (delta_time) * (delta_time) );
+}
+
+const handlePysics = function(delta_time) {
+	gravity(delta_time)
+	if (camera_data.position.y < 5) {
+		camera_data.position.y = 5;
+	}
 }
 
 const handleMovement = function(inputs, controls) {
@@ -97,13 +111,25 @@ const handleMovement = function(inputs, controls) {
 
 let controls = {"w": moveForward, "s": moveBackward, "a": moveLeft, "d": moveRight, "q":moveUp, "e": moveDown}
 let inputs = {"w": false, "s": false, "a": false, "d":false, "q":false, "e":false}
+let start = Date.now()
+let prev_time = start
+let delta = Date.now() - prev_time;
 function animate() {
+	light.position.set(camera_data.position.x, camera_data.position.y + 10, camera_data.position.z);
+
 	resizeCanvasToDisplaySize();
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
-	torus.rotateOnAxis(new THREE.Vector3(0, 0, 1), 0.01);
+	delta = Date.now() - prev_time;
+	prev_time = Date.now();
 	handleMovement(inputs, controls);
-};
+	handlePysics(delta / 1000)
+	camera.position.x = camera_data.position.x;
+	camera.position.y = camera_data.position.y;
+	camera.position.z = camera_data.position.z;
+
+	console.log(camera.position)
+	renderer.render( scene, camera );
+	requestAnimationFrame( animate );
+}; 
 
 
 animate();
